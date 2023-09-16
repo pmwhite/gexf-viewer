c89643ccd671059aa89caa95307d8db2a6864b3e
Philip White <philipwhite@cedarville.edu>
2023-09-15 22:13:33 -0400

    Add canvas and start rendering some nodes

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 const file_picker = document.getElementById(`file-picker`);
+const canvas = document.getElementById(`canvas`);
 
 // A graph is a collection of node objects keyed by an id. Each node contains a
 // label and a collection of edges to or from other nodes.
 let nodes = {};
+
+const render = () => {
+  const width = window.innerWidth - 20;
+  const height = window.innerHeight - 100;
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = `rgb(0, 0, 0)`;
+  ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = `rgb(255, 255, 255)`;
+
+  let root;
+  for (const node of Object.values(nodes)) {
+    if (!root || node.height > root.height) {
+      root = node;
+    }
+  }
+
+  let center = width / 2;
+  if (root) {
+    ctx.fillRect(center - 2, 10, 4, 4);
+
+    let len = root.outward.length;
+    let i = 0;
+    let spacing = width / len;
+    for (let child of root.outward) {
+      const x = center - 2 - (spacing / 2 * (len - 1)) + i * spacing;
+      ctx.fillRect(x, 50, 4, 4);
+      if (child.label) {
+        const text_x = x - ctx.measureText(child.label).width / 2;
+        ctx.fillText(child.label, text_x, 70);
+      }
+      i += 1;
+    }
+  }
+};
+
+const compute_heights = () => {
+  const iter = node => {
+    if (!node.height) {
+      let max = -1;
+      for (const other of node.outward) {
+        iter(other);
+        if (other.height > max) { max = other.height; }
+      }
+      node.height = max + 1;
+    }
+  };
+  for (const node of Object.values(nodes)) {
+    iter(node);
+  }
+};
 
 // [load_file] clears the current graph and re-populates it with the contents
 // of the file currently picked by the file-picker. If nothing is picked, then
@@ -10,37 +63,40 @@ let nodes = {};
 const load_current_file = () => {
   const [ file ] = file_picker.files;
   nodes = {};
-  file.text().then(text => {
-    let parser = new DOMParser();
-    let xml = parser.parseFromString(text, `application/xml`);
+  if (file) {
+    file.text().then(text => {
+      let parser = new DOMParser();
+      let xml = parser.parseFromString(text, `application/xml`);
 
-    let node_iterator = xml.evaluate(
-      `/gexf/graph/nodes/node`, xml, null, XPathResult.ANY_TYPE, null);
-    let node;
-    while ((node = node_iterator.iterateNext())) {
-      let id = parseInt(node.getAttribute(`id`));
-      let x = {
-        id: id,
-        label: node.getAttribute(`label`),
-        edges: []
-      };
-      nodes[id] = x;
-    }
+      let node_iterator = xml.evaluate(
+        `/gexf/graph/nodes/node`, xml, null, XPathResult.ANY_TYPE, null);
+      let node;
+      while ((node = node_iterator.iterateNext())) {
+        let id = parseInt(node.getAttribute(`id`));
+        let x = {
+          id: id,
+          label: node.getAttribute(`label`),
+          outward: []
+        };
+        nodes[id] = x;
+      }
 
-    let edge_iterator = xml.evaluate(
-      `/gexf/graph/edges/edge`, xml, null, XPathResult.ANY_TYPE, null);
-    let edge;
-    while ((edge = edge_iterator.iterateNext())) {
-      let source = parseInt(edge.getAttribute(`source`));
-      let target = parseInt(edge.getAttribute(`target`));
-      let x = { source: source, target: target };
-      nodes[source].edges.push(x);
-      nodes[target].edges.push(x);
-    }
-    let num_nodes = Object.keys(nodes).length;
-    console.log(`Loaded graph. Found ${num_nodes} nodes.`);
+      let edge_iterator = xml.evaluate(
+        `/gexf/graph/edges/edge`, xml, null, XPathResult.ANY_TYPE, null);
+      let edge;
+      while ((edge = edge_iterator.iterateNext())) {
+        let source = parseInt(edge.getAttribute(`source`));
+        let target = parseInt(edge.getAttribute(`target`));
+        nodes[source].outward.push(nodes[target]);
+      }
+      let num_nodes = Object.keys(nodes).length;
+      console.log(`Loaded graph. Found ${num_nodes} nodes.`);
 
-  });
+      compute_heights();
+      render();
+
+    });
+  }
 };
 
 load_current_file();
